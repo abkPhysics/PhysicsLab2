600b2ceecdfce9e410c2fd9eba429ed2748035a1
Abhi <mariandrive22@gmail.com>
2013-07-20 11:25:52 -0400

    Added Script

--- ElectroStatics2/curvefit.py
+++ ElectroStatics2/curvefit.py
@@ -2,6 +2,7 @@
 from sklearn import datasets, linear_model
 from pylab import *
 import numpy as np
+import matplotlib.pyplot as plt
 
 regr = linear_model.LinearRegression() #fit_intercept = False)
 
@@ -10,6 +11,7 @@ volts = np.array([750,1500,3000,6000])
 x=np.log(volts)
 
 y = charges = np.array([1.9,2.9,4.1,5.8])
+chargesExpected = np.array([3.75,7.5,15,30])
 
 
 #x=np.arange(0,10, .1)
@@ -27,9 +29,11 @@ print fit.intercept_
 
 ion()
 clf()
-#scatter(volts,charges)
-#scatter(np.log(volts),charges)
-scatter(volts,y)
+
+
+
+scatter(volts,y,c = 'r')
+scatter(volts,chargesExpected,c = 'b')
 px = np.arange(0.1,np.max(volts) + .1, .1)
 plot(px, fit.coef_[0]*(np.log(px)) + fit.intercept_)
 show()
